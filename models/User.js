82a0a379b962832df576571ac54738541aa6c33f
Nicholas Easter <nicholas.easter17@gmail.com>
2022-02-16 18:45:54 -0800

    hashed passwords on create and update and allowed the user to login by comparing the plaintext password to the hashed password

--- models/User.js
+++ models/User.js
@@ -1,8 +1,13 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define table columns and configuration
 User.init(
@@ -45,6 +50,17 @@ User.init(
       }
     },
     {
+      hooks: {
+        // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+        async beforeUpdate(updateUserData) {
+          newUserData.password = await bcrypt.hash(updateUserData.password, 10);
+          return newUserData;
+        }
+      },
       sequelize,
       timestamps: false,
       freezeTableName: true,
